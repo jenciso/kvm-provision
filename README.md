8995c001b3d3b09cf10fc4c514b5a1fd75c9f4a0
Juan Enciso <juan.enciso@gmail.com>
2021-01-02 00:24:12 -0300

    Add amazon linux 2 and centos8

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## PRE-REQUISITES
 
-Before to use this tool, you need to install these packages (Ubuntu host):
+This code run on an Ubuntu 18 host machine. It need to have installed these packages:
 
 ```shell
 sudo apt-get install qemu qemu-kvm libvirt-bin bridge-utils virt-manager libguestfs-tools
@@ -10,11 +10,16 @@ sudo service libvirtd start
 sudo update-rc.d libvirtd enable
 ```
 
-Also, in order to create virtual machines, you should have a iso image or a qcow2 cloud image. We prefer centos7 cloud image. To download it, run:
+In order to create virtual machines, you should have a iso image or a qcow2 cloud image. We prefer centos7 cloud image. To download it, run:
 
 ```shell
-sudo wget http://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2 \
-  -O /var/lib/libvirt/boot/CentOS-7-x86_64-GenericCloud.qcow2 
+URL_QCOW2_CENTOS7=http://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2
+URL_QCOW2_CENTOS8=https://cloud.centos.org/centos/8/x86_64/images/CentOS-8-GenericCloud-8.3.2011-20201204.2.x86_64.qcow2
+URL_QCOW2_AMAZON2=https://cdn.amazonlinux.com/os-images/2.0.20201111.0/kvm/amzn2-kvm-2.0.20201111.0-x86_64.xfs.gpt.qcow2
+
+sudo -E wget -P /var/lib/libvirt/boot/ $URL_QCOW2_CENTOS7
+sudo -E wget -P /var/lib/libvirt/boot/ $URL_QCOW2_CENTOS8
+sudo -E wget -P /var/lib/libvirt/boot/ $URL_QCOW2_AMAZON2
 ```
 
 ## GETTING STARTED
@@ -30,7 +35,7 @@ Example: creating a VM with the name `centos7`
 Or using some arguments:
 
 ```
-./new-vm.sh -n centos7 -m 1024 -c 2 -i 192.168.122.11
+./new-vm.sh -n centos7 -m 1024 -c 2 -i 192.168.122.11 -t centos7
 ```
 
 Post-installation steps:
@@ -58,11 +63,7 @@ This command will create the `/dev/vdb` device with 60GB
 ./add-disk.sh -n centos7 -d vdb -s 60G
 ```
 
-* How to get the centos7 cloud image using a slower internet connection?
+## References
 
-If your download is slower, you could use `-c` flag to continue downloading to get after each broke connections
+https://github.com/giovtorres/kvm-install-vm
 
-```shell
-sudo wget -c http://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2 \
-  -O /var/lib/libvirt/boot/CentOS-7-x86_64-GenericCloud.qcow2
-```
--- new-vm.sh
+++ new-vm.sh
@@ -9,7 +9,7 @@ die () {
 
 # Function: Print a help message.
 usage() { 
-  echo "Usage: $0 [ vm_name ] [ -n vm_name ] [ -m memory_mb ] [ -c num_cpus ] [ -i ip ]" 1>&2 
+  echo "Usage: $0 [ vm_name ] [ -n vm_name ] [ -m memory_mb ] [ -c num_cpus ] [ -i ip ] [ -s disk_size ] [ -q qcow2_image ] [ -t distro ]" 1>&2 
 }
 
 ## Exports vars from config file
@@ -18,30 +18,74 @@ source config.conf
 set +o allexport
 
 ## Use the arguments passed to script file
-while getopts n:m:c:i:h option ; do
+while getopts n:m:c:i:s:q:t:h option ; do
   case "${option}" in 
     n) VM=${OPTARG};;
     m) VM_MEM=${OPTARG};;
     c) VM_CPU=${OPTARG};;
     i) IPADDR=${OPTARG};;
+    s) DISK_SIZE=${OPTARG};;
+    q) QCOW2_IMAGE=${OPTARG};;
+    t) DISTRO=${OPTARG};;
     h) usage && exit;;
     *) VM=${OPTARG};;
   esac
 done
 
+## Initialize some vars
+
+    case "${DISTRO}" in
+        amazon2)
+            export QCOW2_IMAGE=amzn2-kvm-2.0.20201111.0-x86_64.xfs.gpt.qcow2
+            export OS_VARIANT="rhel8.0"
+            export LOGIN_USER=ec2-user
+            ;;
+        centos8)
+            export QCOW2_IMAGE=CentOS-Stream-GenericCloud-8-20201217.0.x86_64.qcow2
+            export OS_VARIANT="rhel8.0"
+            export LOGIN_USER=centos
+            ;;
+        centos7)
+            export QCOW2_IMAGE=CentOS-7-x86_64-GenericCloud.qcow2
+            export OS_VARIANT="centos7.0"
+            export LOGIN_USER=centos
+            ;;
+        ubuntu)
+            export QCOW2_IMAGE=ubuntu-18.04-server-cloudimg-amd64.img
+            export OS_VARIANT="ubuntu18.04"
+            export LOGIN_USER=ubuntu
+            ;;
+        *)
+            die "${DISTRO} not a supported OS.  Specifiy a linux distro using -t. Ex '-t centos7'."
+            ;;
+    esac
+
 ## Creating meta and user data
 TPM_WORKDIR=`mktemp -d -t XXX`
 
-cat templates/user-data.template | envsubst > ${TPM_WORKDIR}/user-data
+if [[ ${DHCP_CLIENT} == "true" ]]; then
+  cat templates/user-data-dhcp.template | envsubst > ${TPM_WORKDIR}/user-data
+else
+  cat templates/user-data.template | envsubst > ${TPM_WORKDIR}/user-data
+fi
+
 cat templates/meta-data.template | envsubst > ${TPM_WORKDIR}/meta-data
 
-## Initializing configs
 sudo mkdir -p ${DATA_DIR}/$VM
-sudo cp /var/lib/libvirt/boot/CentOS-7-x86_64-GenericCloud.qcow2 ${DATA_DIR}/$VM/$VM.qcow2
+sudo cp /var/lib/libvirt/boot/${QCOW2_IMAGE} ${DATA_DIR}/$VM/$VM.qcow2
 export LIBGUESTFS_BACKEND=direct
-sudo qemu-img create -f qcow2 -o preallocation=off ${DATA_DIR}/$VM/$VM.new.image ${DISK_SIZE}
-sudo virt-resize --quiet --expand /dev/sda1 ${DATA_DIR}/$VM/$VM.qcow2 ${DATA_DIR}/$VM/$VM.new.image
-sudo mv -f ${DATA_DIR}/$VM/$VM.new.image ${DATA_DIR}/$VM/$VM.qcow2
+
+case "${DISTRO}" in
+  ubuntu*|amazon2)
+    sudo qemu-img resize ${DATA_DIR}/$VM/$VM.qcow2 ${DISK_SIZE}
+    ;;
+  *)
+    sudo qemu-img create -f qcow2 -o preallocation=off ${DATA_DIR}/$VM/$VM.new.image ${DISK_SIZE}
+    sudo virt-resize --quiet --expand /dev/sda1 ${DATA_DIR}/$VM/$VM.qcow2 ${DATA_DIR}/$VM/$VM.new.image
+    sudo mv -f ${DATA_DIR}/$VM/$VM.new.image ${DATA_DIR}/$VM/$VM.qcow2
+    ;;
+esac
+
 sudo mkisofs -o ${DATA_DIR}/$VM/$VM-cidata.iso -V cidata -J --input-charset iso8859-1 -r ${TPM_WORKDIR}/user-data ${TPM_WORKDIR}/meta-data
 rm -f ${TPM_WORKDIR}/user-data ${TPM_WORKDIR}/meta-data
 
@@ -55,12 +99,14 @@ sudo virt-install --import --name $VM \
 --disk ${DATA_DIR}/$VM/$VM-cidata.iso,device=cdrom \
 --network ${KVM_NETWORK_MODE},model=virtio \
 --os-type=linux \
---os-variant=centos7.0 \
+--os-variant=${OS_VARIANT} \
 --graphics spice \
 --noautoconsole
 
 ## Automatically startup when the host machine initialize its boot process
-sudo virsh autostart $VM
+if [[ ${AUTOSTART} == "true" ]]; then
+  sudo virsh autostart $VM
+fi
 
 ## Eject iso media. This change will be applied in the next reboot
 sudo virsh change-media $VM hda --eject --config
